7422f24527a041458b6b6b3e36329945e62e1421
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-05-27 12:41:51 +0200

    Implement: Take a .csv file transform it on Python Dict of list  and transform the python dict/list  into a list into in .JSON file

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    try:
+        with open(filename, "r") as file:
+            reader = csv.DictReader(file)
+            data = list(reader)
+        with open("data.json", "w") as json_file:
+            json.dump(data, json_file, indent=4)
+            return True
+    except Exception:
+        return False
