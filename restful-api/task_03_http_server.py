4c50307e5da4e9ee613faf4b593e479f677899e5
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-06-14 09:59:55 +0200

    Add: Documentation for correction

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,10 +1,15 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+#!/usr/bin/python3
 """Simple HTTP server exposing API endpoints."""
+
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
 
 class MyHandler(BaseHTTPRequestHandler):
     """Handle GET requests for API endpoints."""
 
     def do_GET(self):
+        """Handle GET requests."""
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello")
