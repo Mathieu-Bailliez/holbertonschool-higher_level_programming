1113e8dfcdddc4bec7f208727175c221234d3709
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-06-14 09:56:24 +0200

    Implement basic REST API with http.server

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,17 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello")
+
+
+server = HTTPServer(
+    ("localhost", 8000),
+    MyHandler
+)
+
+
+server.serve_forever()
